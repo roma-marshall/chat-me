4b01e3c209deaf8fc5378ac2b450adc1f1e2e64d
roma_marshall <roma_marshall@outlook.com>
2024-10-31 19:31:23 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col container justify-center mt-20 mx-auto space-y-4">
+    <div class="border h-40">
+      <div v-for="item in messages">
+        {{ item }}
+      </div>
+    </div>
+    <input @keyup.enter="sendMessage" v-model="message" type="text" class="border">
+    <div class="flex justify-center">
+      <button
+          @click="sendMessage"
+          class="bg-black text-white py-2 px-5 rounded">send message</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('')
+const messages = ref(['hello', 'world'])
+
+const sendMessage = () => {
+  messages.value.unshift(message.value)
+  message.value = ''
+}
+
+</script>
