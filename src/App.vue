3e36f4cb9c5bcd34cccb68c9fab9d7d5b7997e4d
roma_marshall <roma_marshall@outlook.com>
2024-10-31 20:27:38 +0100

    added textarea

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,36 @@
 <template>
   <div class="flex flex-col container justify-center mt-20 mx-auto space-y-4">
-    <div class="flex flex-row justify-center">
-      <div class="border h-96 overflow-auto px-10 py-5 w-full">
-        <div v-for="item in messages" class="border rounded py-4 bg-blue-50 space-y-2 mb-2 space-x-4">
-          <div class="flex justify-start mx-3 space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-8 h-8" viewBox="0 0 24 24" stroke-width="1.5" stroke="#c1c1c1">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-            <div class="flex justify-between w-full my-auto">
-              <span class="text-red-500 font-bold text-sm w-fit">
-                Anonym
-              </span>
-              <span class="text-gray-300 text-sm">
-              {{ item.timestamp }}
+    <div class="border rounded h-96 overflow-auto px-10 py-5 w-full">
+      <div v-for="item in messages" class="border rounded py-4 bg-blue-50 space-y-2 mb-2 space-x-4">
+        <div class="flex justify-start mx-3 space-x-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-8 h-8" viewBox="0 0 24 24" stroke-width="1.5" stroke="#c1c1c1">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          </svg>
+          <div class="flex justify-between w-full my-auto">
+            <span class="text-red-500 font-bold text-sm w-fit">
+              Anonym
             </span>
-            </div>
+            <span class="text-gray-300 text-sm">
+            {{ item.timestamp }}
+          </span>
           </div>
-          <div class="text-gray-500 pr-4">
-            {{ item.message }}
-          </div>
+        </div>
+        <div class="text-gray-500 pr-4">
+          {{ item.message }}
         </div>
       </div>
     </div>
-    <input @keyup.enter="sendMessage" v-model="message" type="text" class="border">
+    <textarea @keyup.enter="sendMessage" v-model="message" type="text" class="border h-20 resize-none py-2 px-4 outline-none rounded" />
     <div class="flex justify-center">
       <button
           @click="sendMessage"
-          class="bg-black text-white py-2 px-5 rounded">send message</button>
+          class="hover:bg-blue-500 bg-black text-white py-2 px-5 rounded">send message</button>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
 
 const id = ref(0)
 const timestamp = ref([])
@@ -44,7 +41,6 @@ const sendMessage = () => {
   const date = new Date()
   timestamp.value.unshift(date.getHours() + ':' + date.getMinutes())
 
-
   messages.value.unshift(
     {
       id: id.value,
@@ -53,19 +49,6 @@ const sendMessage = () => {
     }
   )
   id.value++
-
-
-  // messages.value = {
-  //   id: '1',
-  //   message: 'hello world',
-  //   timestamp: '20:00'
-  // }
-
-
   message.value = ''
-
-
-
 }
-
 </script>
